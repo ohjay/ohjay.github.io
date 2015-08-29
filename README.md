9031d1623f79d5a2b033a051b83f59cbb9d30d36
Owen Jow <owenjow@berkeley.edu>
2015-08-29 12:52:59 -0700

    Updated many miscellaneous aesthetics

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ There's not much to say here. Since a website is worth a thousand words, you can
 This is the personal website of Owen Michael Jow, in which Owen Michael Jow will engage in a lot of shameless self-promotion and overall focus on nothing but himself. If that interests you at all, you should ~~get your head examined~~ check it out! Just be sure to grab some popcorn before you do. :)
 
 ### Acknowledgments
-Of course, this undertaking would not have been possible without the indefatigable efforts of the countless others who have gone before. Owen would therefore like to publicize his appreciation for the following libraries, frameworks, and plugins (and credit them for their part in his website!):
+Of course, this undertaking would not have been possible without the indefatigable efforts of countless others who have gone before. Owen would therefore like to publicize his appreciation for the following libraries, frameworks, and plugins (and credit them for their part in his website!):
 
 - [Bootstrap](http://getbootstrap.com/)
 - [Justin Aguilar's CSS3 Animation Sheet](http://www.justinaguilar.com/animations/index.html)
--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
-// Collapse navbar after a selection is made (on mobile)
 if ($(window).width() <= 480 || $(window).height() <= 480) {
+    /* Mobile-specific code */
+    // Remove the "nav navbar-nav" classes from the social link icons
+    $('#links').removeClass('nav navbar-nav');
+            
+    // Replace the footer text with a simpler statement
+    $('#footer-text p').text('Copyright Owen Jow 2015');
+    
+    // Collapse navbar after a selection is made
     $('.nav a').on('click', function(){
         $(".navbar-toggle").click();
     });
-}
-
-// Trigger CSS animations when the element in question is scrolled to
-var addAnimation = function() {
-	var imagePos = $(this).offset().top;
-	var topOfWindow = $(window).scrollTop();
+} else {
+    /* Non-mobile specific code */
+    var addAnimation = function() {
+    	var imagePos = $(this).offset().top;
+    	var topOfWindow = $(window).scrollTop();
     
-	if (imagePos < topOfWindow + 400 || $(window).scrollTop() + $(window).height() > $(document).height() - 50) {
-		$(this).addClass("slideLeft");
-	}
+    	if (imagePos < topOfWindow + 400 || $(window).scrollTop() + $(window).height() > $(document).height() - 50) {
+    		$(this).addClass('slideLeft');
+    	}
+    };
+
+    // Trigger CSS animations when the element in question is scrolled to
+    $(window).scroll(function() {
+    	$('.info-text').each(addAnimation);
+        $('.transp-box-hidden').each(addAnimation);
+    });
 }
 
-$(window).scroll(function() {
-	$('.info-text').each(addAnimation);
-    $('.transp-box-hidden').each(addAnimation);
-    $('.transp-box-proj').each(addAnimation);
+/* Fun for the whole family */
+// Reveal hidden text upon superscript click
+$('#superscript').click(function() {
+    $('#hidden-footnote').addClass('fadeIn');
 });
