3c228df68c60bed1ca70e899cc2b7663124a253f
Owen Jow <owenjow@berkeley.edu>
2015-10-17 17:35:59 -0700

    Configured landing page for mobile devices

--- js/script.js
+++ js/script.js
@@ -3,10 +3,31 @@ var addLinks = function() {
     $('.pipe').text(' | ');
     $('#portfolio').text('Portfolio');
     $('#blog').text('Blog');
-    $('#cs61a').text('CS61A');
+    $('#cs61a').text('CS 61A');
+};
+
+var mobileTransform = function() {
+    if ($(window).width() <= 480) {
+        $("p").css({"width": "90%", "margin-left": "auto", "margin-right": "auto"})
+              .html('. . . . . . . . . . . . . . . . . . . . . . . . . . . . .<br><br>'
+                  + "Hi! Bad news: you've arrived at the landing page for Owen Jow's website. "
+                  + "Since that probably wasn't your intended destination, I've constructed "
+                  + "this as a buffer against the gruesome remainder of this site's contents. "
+                  + "If you possess any vestige of control over your mental facilities, you'll turn back now.<br><br>"
+                  + "That was your final warning.<br><br>"
+                  + '<a href="portfolio.html" id="portfolio">Portfolio</a>&nbsp;&nbsp;|&nbsp;&nbsp;'
+                  + '<a href="blog.html" id="blog">Blog</a>&nbsp;&nbsp;|&nbsp;&nbsp;'
+                  + '<a href="cs61a.html" id="cs61a">CS 61A</a><br>'
+                  + '. . . . . . . . . . . . . . . . . . . . . . . . . . . . .');
+    }
 };
 
 $(document).ready(function() {
+    mobileTransform();
+    $(window).resize(function() {
+        mobileTransform();
+    });
+    
     $('.animated-text').typed({
         strings: [
             "Don't you miss the quiet of a simpler age?", 
