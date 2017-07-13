a1b72b9062324ae1a462b6aec6bb717756d5b816
Owen Jow <owenjow@berkeley.edu>
2017-07-12 23:57:55 -0700

    Preparation for a few future Cornell notes

---
+++ js/flickering.js
@@ -0,0 +1,41 @@
+const HASHES = [
+    'U2FsdGVkX1+E3ye2WKz2JfO33ZjtmQx09FeN5v7hL2JGdG0zB5WeTQ6FOnaR93oKEWlqFzwpDY9Qkt45Ls+2xA=='
+]
+const NUM_HASHES = HASHES.length;
+
+function verify(id, accessCode) {
+    var token = id.replace(/\s+/g, '').toLowerCase() + accessCode;
+    for (var i = 0; i < NUM_HASHES; ++i) {
+        var result = CryptoJS.AES.decrypt(HASHES[i], token).toString(CryptoJS.enc.Latin1);
+        if (/^https?:\/\/[^\s]+$/.test(result)) {
+            window.location = result;
+            return true;
+        }
+    }
+    
+    // Display denial
+    document.getElementById("lit-header").className = "";
+    document.getElementById("go").className = "denied";
+    document.getElementById("go").value = "Access Denied";
+    
+    return false;
+}
+
+$(document).ready(function() {
+    var state = false;
+    var numAttempts = 0;
+
+    $('#access-panel').on('submit', function(evt) {
+        evt.preventDefault();
+        if (numAttempts < 10) {
+            document.getElementById("lit-header").className = "power-on";
+            document.getElementById("go").className = "";
+            document.getElementById("go").value = "Verifying...";
+            
+            numAttempts += 1;
+            setTimeout(function() {
+                verify($('#name').val(), $('#password').val());
+            }, 2000);
+        }
+    });
+});
--- js/restricted.js
+++ js/restricted.js
@@ -3,15 +3,19 @@ $(document).ready(function() {
     var hash = 'U2FsdGVkX1/0yqSyURFbIDOIvJ5TuGPskR76xBbrDEXCBnoXnUTxmVdsmgHS3Hiqef/CdbAi11/P4T4Z7SJObw==';
     var numAttempts = 0;
     var result = null;
-                    
+
     $(window).bind("keypress", function(evt) {
         if (typed.length < 65 && numAttempts < 65) {
             typed += String.fromCharCode(evt.keyCode);
             result = CryptoJS.AES.decrypt(hash, typed).toString(CryptoJS.enc.Latin1);
             if (/^http:[a-z\.\/]{10,}\/$/.test(result)) {
                 window.location = result;
+            } else if (Math.random() < 0.005) {
+                window.location = '/restricted/access';
             }
             numAttempts += 1;
+        } else if (Math.random() < 0.01) {
+            window.location = '/restricted/access';
         }
     });
 });
